0cbd177905fb504894b19fce84df7a30aa4db901
Adrian Maulani <maulani.ad@gmail.com>
2024-09-18 00:14:48 +0700

    chore: changed imports

--- database/repositories/__init__.py
+++ database/repositories/__init__.py
@@ -1,8 +1,5 @@
 """Centralized place for pre-defined query functions used to retrieve data from the database."""
 
 
-from database.repositories.profile import ProfileRepo
-from database.repositories.user import UserRepo
-
-
-__all__ = ["ProfileRepo", "UserRepo"]
+from database.repositories.profile import ProfileRepo  # noqa: F401
+from database.repositories.user import UserRepo        # noqa: F401
