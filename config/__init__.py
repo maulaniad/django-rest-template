c8c6f78d759b941016843e1b09158ab3ae5c23f9
Adrian Maulani <maulani.ad@gmail.com>
2024-09-25 13:30:39 +0700

    fix: removed import line causing Circular Import

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,3 @@
-# Import objects if needed in settings.py to avoid circular import error
-from helpers.response import ResponseRenderer    # noqa: F401
-
-
 # Setup celery app for celery_beat
 from config.celery import app as celery_app
 
