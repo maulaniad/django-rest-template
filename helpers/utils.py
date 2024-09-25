5704fd1110afbf3ebe6c03224c01af3edb4cda8e
Adrian Maulani <maulani.ad@gmail.com>
2024-09-25 13:32:19 +0700

    feat: utilities to send email and otp generation

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,11 +1,15 @@
 from fastnanoid import generate
 from jwt import encode
+from random import randint
 from typing import Any
 
 from django.conf import settings
 from django.utils.timezone import now, timedelta
 
 from rest_framework.utils.serializer_helpers import ReturnDict
+
+from helpers.types import EmailMessage
+from tasks.task_email import task_send_email
 
 
 def generate_oid(length: int = 21):
@@ -28,3 +32,19 @@ def generate_token(data: dict[str, Any] | ReturnDict[str, Any]):
     )
 
     return encoded_token
+
+
+def generate_otp():
+    """Generates OTP with given length."""
+    return randint(100000, 999999)
+
+
+def send_email(email: EmailMessage, fail_silently: bool = False):
+    """Customized function to send an email, asynchronously."""
+    task_send_email.delay(
+        subject=email.subject,
+        body=email.body,
+        from_email=email.from_email,
+        to=email.to,
+        fail_silently=fail_silently
+    )
