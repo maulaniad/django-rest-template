a24d584022f32f007cef72928771e07758baaf72
Adrian Maulani <maulani.ad@gmail.com>
2024-09-27 09:00:02 +0700

    refactor: distinct serializer names
    Make serializer name distinctive for validation (Validate prefix) and transformation (Serializer suffix)

--- api/authentication/serializers.py
+++ api/authentication/serializers.py
@@ -1,16 +1,22 @@
 from rest_framework.serializers import (Serializer,
+                                        ModelSerializer,
                                         BooleanField,
                                         CharField,
                                         DateTimeField,
                                         IntegerField)
 
 
-class LoginPayloadSerializer(Serializer):
+class ValidateLoginPayload(Serializer):
     username = CharField(max_length=50, required=True)
     password = CharField(required=True)
 
 
-class UserProfileDataSerializer(Serializer):
+class ValidateVerifyOTPPayload(Serializer):
+    access_id = CharField(max_length=21, required=True)
+    otp = IntegerField(required=True)
+
+
+class ProfileDataSerializer(ModelSerializer):
     oid = CharField(max_length=21)
     date_created = DateTimeField()
     date_updated = DateTimeField()
@@ -19,14 +25,9 @@ class UserProfileDataSerializer(Serializer):
     phone = CharField(max_length=15, allow_blank=True)
 
 
-class UserDataSerializer(Serializer):
+class UserDataSerializer(ModelSerializer):
     username = CharField(max_length=50, required=True)
     email = CharField(max_length=50, required=True)
     is_active = BooleanField(default=True)
     last_login = DateTimeField(allow_null=True)
-    profile = UserProfileDataSerializer()
-
-
-class VerifyOTPPayloadSerializer(Serializer):
-    access_id = CharField(max_length=21, required=True)
-    otp = IntegerField(required=True)
+    profile = ProfileDataSerializer()
